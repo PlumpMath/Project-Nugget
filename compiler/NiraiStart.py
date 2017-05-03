ef409e0ce32a9bdc72d51faf0cdbd463fdcde588
thetestgame <jordanmax@nxt-studios.com>
2017-05-03 14:40:57 -0500

    compiler: Added game compiler based on Nirai

---
+++ compiler/NiraiStart.py
@@ -0,0 +1,28 @@
+from panda3d.core import *
+import aes
+
+import niraidata
+
+# Config
+prc = niraidata.CONFIG
+iv, key, prc = prc[:16], prc[16:32], prc[32:]
+prc = aes.decrypt(prc, key, iv)
+
+for line in prc.split('\n'):
+    line = line.strip()
+    if line:
+        loadPrcFileData('nirai config', line)
+
+# Mount resources.mf
+vfs = VirtualFileSystem.getGlobalPtr()
+vfs.mount('resources.mf', '.', 0)
+
+# Run
+try:
+    import main
+
+except SystemExit:
+    pass
+
+except:
+    raise
---
+++ compiler/make.py
@@ -0,0 +1,123 @@
+from direct.distributed.PyDatagram import PyDatagram
+import argparse
+import sys
+import os
+
+if not os.path.exists('../built'):
+    os.mkdir('../built')
+
+sys.path.append('nirai/src')
+
+from panda3d.core import Datagram
+from niraitools import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--compile-cxx', '-c', action='store_true',
+                    help='Compile the CXX codes and generate nugget.exe into built.')
+parser.add_argument('--make-nri', '-n', action='store_true',
+                    help='Generate nuggets.nri.')
+parser.add_argument('--resources', '-m', action='store_true',
+                    help='Pack resources.mf.')
+args = parser.parse_args()
+
+def niraicall_obfuscate(code):
+    # We'll obfuscate if len(code) % 4 == 0
+    # This way we make sure both obfuscated and non-obfuscated code work.
+    if len(code) % 4:
+        return False, None
+
+    # There are several ways to obfuscate it
+    # For this example, we'll invert the string
+    return True, code[::-1]
+
+niraimarshal.niraicall_obfuscate = niraicall_obfuscate
+
+class SamplePackager(NiraiPackager):
+    HEADER = 'NUGGET'
+    BASEDIR = '..' + os.sep
+
+    def __init__(self, outfile):
+        NiraiPackager.__init__(self, outfile)
+        self.__manglebase = self.get_mangle_base(self.BASEDIR)
+
+        self.add_panda3d_dirs()
+        self.add_default_lib()
+        self.add_directory(self.BASEDIR, mangler=self.__mangler)
+
+    def __mangler(self, name):
+        # N.B. Mangler can be used to strip certain files from the build.
+        # The file is not included if it returns an empty string.
+
+        return name[self.__manglebase:].strip('.')
+
+    def generate_niraidata(self):
+        print 'Generating niraidata'
+
+        config = self.get_file_contents(self.BASEDIR + 'config' + os.sep + 'release.prc', True)
+        niraidata = 'CONFIG = %r' % config
+        self.add_module('niraidata', niraidata, compile=True)
+
+    def process_modules(self):
+        '''
+        This method is called when it's time to write the output.
+
+        For nuggets.nri, we use an encrypted datagram.
+        The datagram is read by core.cxx, which populates Python frozen array.
+
+        Datagram format:
+
+        uint32 numModules
+        for each module:
+            string name
+            int32 size *
+            data(abs(size))
+
+        * Negative size means the file was an __init__
+        '''
+
+        dg = PyDatagram()
+        dg.addUint32(len(self.modules))
+
+        for moduleName in self.modules:
+            data, size = self.modules[moduleName]
+
+            dg.addString(moduleName)
+            dg.addInt32(size)
+            dg.appendData(data)
+
+        data = dg.getMessage()
+
+        iv = '\0' * 16
+        key = 'AmazingNuggetKey'
+        return aes.encrypt(data, key, iv)
+
+if args.compile_cxx:
+    if sys.platform == 'win32':
+        output = 'nugget.exe'
+
+    elif sys.platform == 'darwin':
+        output = 'Project Nugget'
+
+    else:
+        raise Exception('The platform "%s" in use is not supported.' % sys.platform)
+
+    compiler = NiraiCompiler(output)
+    compiler.add_nirai_files()
+    compiler.add_source('engine/core.cxx')
+    compiler.run()
+
+if args.make_nri:
+    pkg = SamplePackager('../built/nuggets.nri')
+    pkg.add_file('NiraiStart.py')
+    pkg.generate_niraidata()
+    pkg.write_out()
+
+if args.resources:
+    os.chdir('../')
+    cmd = 'multify -cf built/resources.mf resources'
+    p = subprocess.Popen(cmd, stdout=sys.stdout, stderr=sys.stderr, shell=True)
+    v = p.wait()
+
+    if v != 0:
+        print 'The following command returned non-zero value (%d): %s' % (v, cmd[:100] + '...')
+        sys.exit(1)
